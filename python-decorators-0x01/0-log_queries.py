52c73780ad607e018becea9a91cb4ec3076dc9f1
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-05-25 00:28:26 +0100

    0. Logging database Queries update

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,6 +1,29 @@
 import sqlite3
 import functools
 
+
+
+# Decorator to log SQL queries
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        query = kwargs.get('query') or (args[0] if args else None)
+        if query:
+            print(f"[SQL LOG] Executing query: {query}")
+        else:
+            print("[SQL LOG] No query found in arguments.")
+        return func(*args, **kwargs)
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
 def test():
 
     # Connect to a database (or create it if it doesn't exist)
@@ -28,28 +51,6 @@ def test():
     # Commit changes and close the connection
     conn.commit()
     conn.close()
-
-
-# Decorator to log SQL queries
-def log_queries(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        query = kwargs.get('query') or (args[0] if args else None)
-        if query:
-            print(f"[SQL LOG] Executing query: {query}")
-        else:
-            print("[SQL LOG] No query found in arguments.")
-        return func(*args, **kwargs)
-    return wrapper
-
-@log_queries
-def fetch_all_users(query):
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute(query)
-    results = cursor.fetchall()
-    conn.close()
-    return results
 
 
 test()

8e0a72edb7b6a268479e5f966f0c0cb3374d4f4d
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-05-31 21:11:42 +0100

    views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your views here.
+from .models import Conversation, Message, Status
+from .serializers import ConversationSerializer, MessageSerializer, StatusSerializer
+
+# ViewSet for Conversations
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+
+    def get_queryset(self):
+        return self.queryset.filter(participants=self.request.user)
+
+    def perform_create(self, serializer):
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+
+# ViewSet for Messages
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        conversation_id = self.request.query_params.get('conversation')
+        if conversation_id:
+            return self.queryset.filter(conversation__id=conversation_id, conversation__participants=self.request.user)
+        return self.queryset.none()
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+# ViewSet for Statuses
+class StatusViewSet(viewsets.ModelViewSet):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(created_at__gte=timezone.now() - timedelta(hours=24))
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)

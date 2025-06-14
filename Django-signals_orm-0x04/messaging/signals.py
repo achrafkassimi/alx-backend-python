84503c30346f55d6bdb021fc75978fcf59581492
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-06-14 20:17:32 +0100

    edited_at

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory
+from django.utils import timezone
+
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
@@ -22,6 +24,8 @@ def save_message_history(sender, instance, **kwargs):
                 message=original,
                 old_content=original.content
             )
+            instance.edited_at = timezone.now()  # Set the timestamp when it was edited
+            instance.edited_by = instance.sender  # Assuming the sender is editing the message, you can modify as needed
             instance.edited = True
     except Message.DoesNotExist:
         pass  # Pas de message original, donc rien à faire

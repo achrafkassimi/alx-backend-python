9d6bb059c813553967d12bc3b9007937f626ce0f
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-06-15 17:09:31 +0100

    Checks for prefetchrelated and selectrelated 2

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -14,6 +14,7 @@ def delete_user(request):
     user.delete()
     return redirect('login')
 
+
 def conversation_thread(request):
     # On récupère tous les messages racines
     messages = Message.objects.filter(parent_message__isnull=True).select_related('sender', 'receiver').prefetch_related(
@@ -21,18 +22,26 @@ def conversation_thread(request):
     )
     return render(request, 'messages/thread.html', {'messages': messages})
 
+
 def view_message_thread(request, message_id):
     root = Message.objects.select_related('sender', 'receiver').get(id=message_id)
     thread = get_thread(root)
     return render(request, 'messages/thread_detail.html', {'message': root, 'thread': thread})
 
 
-@login_required
-def unread_messages_view(request):
-    sender = request.user
-    unread_msgs = Message.unread.for_user(sender)
-    return render(request, 'messages/unread.html', {'messages': unread_msgs})
+# @login_required
+# def unread_messages_view(request):
+#     sender = request.user
+#     unread_msgs = Message.unread.for_user(sender)
+#     return render(request, 'messages/unread.html', {'messages': unread_msgs})
 
+@login_required
+def user_messages_view(request):
+    messages = Message.objects.filter(sender=request.user)\
+        .select_related('receiver')\
+        .prefetch_related('history', 'replies')  # Assuming Message has related_name="replies"
+
+    return render(request, 'messaging/user_messages.html', {'messages': messages})
 
 # Par exemple, ta vue qui affiche la liste des messages d'une conversation :
 @cache_page(60)  # 60 secondes de cache
